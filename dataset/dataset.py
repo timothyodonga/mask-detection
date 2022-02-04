7a9e16ee16cb091a438afc90a612f17e4a4ce6ce
timothyodonga <timothy.odonga@gmail.com>
2022-02-04 10:38:05 +0300

    Fixed error in loading data into dataset class

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,13 +2,14 @@ import os
 import torch
 import torch.utils.data
 from PIL import Image
+import pandas as pd
 
 
 class MyDataset(torch.utils.data.Dataset):
     def __init__(self, root, img_list, transforms=None):
         self.root = root
         self.transforms = transforms
-        self.img_list = img_list
+        self.img_list = pd.read_csv(img_list)
 
     def __len__(self):
         return len(self.img_list)
@@ -22,9 +23,9 @@ class TrainDataset(MyDataset):
         super().__init__(root, img_list, transforms)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.root, self.img_list[idx][0])
+        img_path = os.path.join(self.root, self.img_list["image"][idx])
         img = Image.open(img_path).convert("RGB")
-        label = self.img_list[idx][1]
+        label = self.img_list["target"][idx]
 
         if self.transforms is not None:
             img = self.transforms(img)
